772ea452d792295cc8f9f780d68df13f5d43aab7
SM Sohag <sohagsheik32@gmail.com>
2023-03-26 17:43:59 +0600

    update modal

--- src/components/SingleData/SingleData.jsx
+++ src/components/SingleData/SingleData.jsx
@@ -9,13 +9,19 @@ const SingleData = ({
   published_in,
 }) => {
   return (
-    <div className="card w-full bg-base-100 shadow-xl">
+    <div className="card w-full bg-base-100 border-2 shadow-xl">
       <figure>
         <img src={image} alt="Shoes" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <h2 className="card-title">{name}</h2>
+        <div>
+          <h1 className="text-warning">Features : </h1>
+          {features.map((item) => (
+            <li>{item}</li>
+          ))}
+        </div>
+        <p>{published_in}</p>
         <div className="card-actions justify-end">
           <label htmlFor="my-modal-5" className="btn btn-accent">
             <span>Go</span>
